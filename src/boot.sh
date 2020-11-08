770704d1e36a0041ee2577ddcc4d9e83d5a1797b
Robbert Langezaal <opicron@users.noreply.github.com>
2020-11-08 22:57:14 +0100

    Update boot.sh

--- src/boot.sh
+++ src/boot.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#set -e
 
 # Mystic BBS docker boot script
 
-#Define cleanup procedure
+# cleanup procedure
 cleanup() {
     echo "Container stopped, performing cleanup..." >> /mystic/logs/mis.log
-    #the stop script is not required-- just for safety
+    
+    # stop server
     /mystic/stop.sh
+    
+    #exit our script (pid 1) thus closing docker correctly
+    exit
 }
 
-#Trap SIGTERM
+# trap SIGTERM
 trap 'cleanup' SIGTERM
 
 if [ "$1" = 'mystic' ]; then
@@ -18,20 +21,18 @@ if [ "$1" = 'mystic' ]; then
     # run cron for fido polling
     cron
     
-    # remove temp file 
-    rm /mystic/semaphore/mis.bsy
+    # run server
+    /mystic/start.sh &
     
-    # run as server on background
-    /mystic/mis server &
+    # because we can not run the server mode
+    while true
+      do sleep 1
+    done    
     
-    # catch PID
-    child=$!
-
-    # removed due to correct handling of sigterm
-    #tail -f /mystic/logs/mis.log
+    # run as server on background, takes waaay to much CPU!
+    # this is this correct solution but in this case inefficient
+    # /mystic/mis server &    
+    
 else
     exec "$@" &
 fi
-
-# Wait for server PID
-wait "$child"
