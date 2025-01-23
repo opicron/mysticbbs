0e5dbf19b8950d1b06db6a5388ee13d91b90db89
Robbert Langezaal <opicron@users.noreply.github.com>
2025-01-23 14:04:45 +0100

    Add files via upload

---
+++ src/tailit.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define the total number of node
+num_nodes=7
+
+# Loop through each node
+for i in $(seq 1 $num_nodes); do
+
+  # Files
+  log_file="/mystic/logs/node${i}.log"
+  login_file="/mystic/temp${i}/login.n${i}"
+  
+  # Log node
+  tail -n0 -F "$log_file" | grep --line-buffered -Evf /mystic/grep_log_blacklist.txt | while IFS= read -r line; do
+    if [ -r "$login_file" ]; then
+      prefix="$(head -n 1 "$login_file"): "
+    else
+      prefix=""
+    fi
+    echo "${prefix}${line}" >> /mystic/logs/nodes.log
+    escaped_line=$(echo "$line" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g')
+    curl -s -o /dev/null -X POST -H "Content-type: application/json" --data "{\"text\":\"${prefix}${escaped_line}\"}" https://hooks.slack.com/services/xxx/xxx
+  done &
+done
+
+# Log errors
+tail -n0 -F /mystic/logs/errors.log | grep --line-buffered -Evf /mystic/grep_log_blacklist.txt | while IFS= read -r line; do
+    prefix="--[ERROR]--"
+    postfix="--[END]--"
+    echo "${prefix}" >> /mystic/logs/nodes.log
+    echo "${line}" >> /mystic/logs/nodes.log
+    echo "${postfix}" >> /mystic/logs/nodes.log
+    escaped_line=$(echo "$line" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g')
+    curl -s -o /dev/null -X POST -H "Content-type: application/json" --data "{'text':\"${escaped_line}\"}" https://hooks.slack.com/services/xxx/xxx
+done &
+
+# Log everything to docker
+tail -n0 -F /mystic/logs/nodes.log >> /proc/1/fd/1 &
