089da1fc93b6ca563ec2631179c63ea8cf8c67b3
Robbert Langezaal <opicron@users.noreply.github.com>
2025-03-23 13:58:24 +0100

    Update tailit.sh

--- src/tailit.sh
+++ src/tailit.sh
@@ -19,19 +19,30 @@ for i in $(seq 1 $num_nodes); do
     fi
     echo "${prefix}${line}" >> /mystic/logs/nodes.log
     escaped_line=$(echo "$line" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g')
-    curl -s -o /dev/null -X POST -H "Content-type: application/json" --data "{\"text\":\"${prefix}${escaped_line}\"}" https://hooks.slack.com/services/xxx/xxx
+    curl -s -o /dev/null -X POST -H "Content-type: application/json" --data "{\"text\":\"${prefix}${escaped_line}\"}" https://hooks.slack.com/services/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
   done &
 done
 
 # Log errors
 tail -n0 -F /mystic/logs/errors.log | grep --line-buffered -Evf /mystic/grep_log_blacklist.txt | while IFS= read -r line; do
-    prefix="--[ERROR]--"
-    postfix="--[END]--"
-    echo "${prefix}" >> /mystic/logs/nodes.log
+    #prefix="--[ERROR]--"
+    #postfix="--[END]--"
+    #echo "${prefix}" >> /mystic/logs/nodes.log
     echo "${line}" >> /mystic/logs/nodes.log
-    echo "${postfix}" >> /mystic/logs/nodes.log
+    #echo "${postfix}" >> /mystic/logs/nodes.log
     escaped_line=$(echo "$line" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g')
-    curl -s -o /dev/null -X POST -H "Content-type: application/json" --data "{'text':\"${escaped_line}\"}" https://hooks.slack.com/services/xxx/xxx
+    curl -s -o /dev/null -X POST -H "Content-type: application/json" --data "{'text':\"${escaped_line}\"}" https://hooks.slack.com/services/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
+done &
+
+# Log mis
+tail -n0 -F /mystic/logs/mis.log | grep --line-buffered -Evf /mystic/grep_log_blacklist.txt | while IFS= read -r line; do
+    #prefix="--[ERROR]--"
+    #postfix="--[END]--"
+    #echo "${prefix}" >> /mystic/logs/nodes.log
+    echo "${line}" >> /mystic/logs/nodes.log
+    #echo "${postfix}" >> /mystic/logs/nodes.log
+    escaped_line=$(echo "$line" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g')
+    curl -s -o /dev/null -X POST -H "Content-type: application/json" --data "{\"text\":\"${prefix}${escaped_line}\"}" https://hooks.slack.com/services/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
 done &
 
 # Log everything to docker
